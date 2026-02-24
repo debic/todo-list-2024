a23b4331069083b69d291ba38ccf4c3d7aae2da7
Debi <debicps@gmail.com>
2026-02-24 11:09:10 +0200

    last details css

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,45 +1,43 @@
-import deleteIcon from '../img/delete.svg';
-import editIcon from '../img/edit.svg';
-import { useDrag, useDrop } from "react-dnd"
+import deleteIcon from "../img/delete.svg";
+import editIcon from "../img/edit.svg";
+import { useDrag, useDrop } from "react-dnd";
 
 export default function PostCard({ task, toDoList, setToDoList, deletePost }) {
-    let typeColor = ''
-    if(task.priority === 'Urgent'){
-        typeColor= 'urgentColor'
-    }else if(task.priority === 'NotUrgent'){
-        typeColor= 'notUrgentColor'
-    }else if(task.priority === 'Bonus'){
-        typeColor= 'bonusColor'
-    }
-   
-    const hanldeDeletePost = () => {
-        deletePost(task.id)
-    }
-    const [{ isDragging }, drag] = useDrag(() => ({
-        type: 'task',
-        item: { id: task.id },
-        collect: (monitor) => ({
-            isDragging: !!monitor.isDragging()
-        })
-    }))
+  let typeColor = "";
+  if (task.priority === "Urgent") {
+    typeColor = "urgentColor";
+  } else if (task.priority === "NotUrgent") {
+    typeColor = "notUrgentColor";
+  } else if (task.priority === "Bonus") {
+    typeColor = "bonusColor";
+  }
 
-    return (
-        <>
-            <div ref={drag} className='card'>
-                <div className='noteContainer'>
-                    <div className={typeColor}>
-                        
-                    </div>
-                    <div>
-                        <p className='titleTask'>{task.text}</p>
-                        <p className='date'>{task.createDate}</p>
-                        <p className='date'>{task.priority}</p>
-                    </div>
-                </div>
-                <div className="deleteIconDiv" onClick={hanldeDeletePost}>
-                    <img src={deleteIcon} alt="Delete icon" className='deleteIcon' />
-                </div>
-            </div>
-        </>
-    )
+  const hanldeDeletePost = () => {
+    deletePost(task.id);
+  };
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "task",
+    item: { id: task.id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  return (
+    <>
+      <div ref={drag} className="card">
+        <div className="noteContainer">
+          <div className={`margin-right ${typeColor}`}></div>
+          <div>
+            <p className="titleTask">{task.text}</p>
+            <p className="date">{task.createDate}</p>
+            <p className="date">{task.priority}</p>
+          </div>
+        </div>
+        <div className="deleteIconDiv" onClick={hanldeDeletePost}>
+          <img src={deleteIcon} alt="Delete icon" className="deleteIcon" />
+        </div>
+      </div>
+    </>
+  );
 }
